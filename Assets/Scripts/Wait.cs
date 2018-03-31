bd638c477d951a7b7dfcd4d4d83c6e752ee83ca7
Max <mjchezem@gmail.com>
2018-03-31 12:33:56 -0400

    3/27 Meeting update
    Animated start screen and linked by buttons

---
+++ Assets/Scripts/Wait.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wait : MonoBehaviour {
+
+    public float seconds;
+    Animator animator;
+
+	// Use this for initialization
+	void Start () {
+
+        animator = gameObject.GetComponent<Animator>();
+        animator.SetBool("animate", false);
+        StartCoroutine("wait");
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator wait() {
+        
+        yield return new WaitForSeconds(seconds);
+        animator.SetBool("animate", true);
+    }
+
+
+}
